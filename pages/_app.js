fcca27ecd906c3da912e9be543ca8573ab981003
VITALIY1986 <staslego1986@gmail.com>
2024-11-01 14:01:11 +0200

    ddddd

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import "../src/styles/main.scss";
 import { ApolloProvider } from "@apollo/client";
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import { SearchProvider } from '../src/components/use-search';
-import { SearchProvider2 } from '../src/components/use-search2';
+
 import client from "../src/components/ApolloClient";
 import { AppProvider } from "../src/components/context/AppContext";
 import { AuthContextProvider } from '../src/components/login-function/auth-context';
@@ -18,13 +17,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthContextProvider>
       <AppProvider>
-        <SearchProvider2 client={client}>
-          <SearchProvider client={client}>
+       
             <ApolloProvider client={client}>
               <Component {...pageProps} />
             </ApolloProvider>
-          </SearchProvider>
-        </SearchProvider2>
+          
       </AppProvider>
     </AuthContextProvider>
   );
